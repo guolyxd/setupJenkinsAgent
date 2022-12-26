05de7ee87cd6b0ec081860d509c9a29860f9adbb
guolyxd <guolyxd@163.com>
2022-12-26 15:13:55 +0800

    Update TestGit.groovy
    update test content

--- jenkinsFile/TestGit.groovy
+++ jenkinsFile/TestGit.groovy
@@ -11,13 +11,12 @@ pipeline {
         }
         stage('Test'){
             steps {
-                sh 'make check'
-                junit 'reports/**/*.xml' 
+                echo 'Testing...'
             }
         }
         stage('Deploy') {
             steps {
-                sh 'make publish'
+                echo 'Deploying...'
             }
         }
     }
